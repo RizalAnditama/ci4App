661208445fda561aef7a61867715dcb41fe54ee6
RizalAnditama <rizalanditama2105@gmail.com>
2022-04-27 15:27:27 +0700

    fix bug gabisa ganti password

---
+++ app/Controllers/Settings.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+use PhpParser\Node\Expr\Isset_;
+
+class Settings extends BaseController
+{
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+
+        $this->session = session();
+        $this->session->start();
+        if (!$this->session->get('username')) {
+            return redirect()->to('/login');
+        }
+    }
+
+    public function index()
+    {
+        helper(['form']);
+        $data = [
+            'title' => 'Settings',
+        ];
+        return view('pages/settings', $data);
+    }
+
+    public function profile()
+    {
+        helper(['form']);
+        $data = [
+            'title' => 'My Profile',
+            'errors' => [],
+            'validation' => $this->validator,
+        ];
+
+        if ($this->request->getMethod() == 'post') {
+            $username = session()->get('username');
+            $hp = session()->get('phone_no');
+            if ($username != $this->request->getVar('username')) {
+                $username = 'required|is_unique[users.username]|alpha_numeric|min_length[3]|max_length[50]';
+            } else {
+                $username = 'required|alpha_numeric|min_length[3]|max_length[50]';
+            }
+
+            if ($hp != $this->request->getVar('phone_no')) {
+                $hp = 'required|is_unique[users.phone_no]|numeric|min_length[7]|max_length[15]';
+            } else {
+                $hp = 'required|numeric|min_length[7]|max_length[15]';
+            }
+
+            if (null !== ($this->request->getVar('newPassword'))) {
+                if (null != ($this->request->getVar('newPassword') || $this->request->getVar('oldPassword'))) {
+                    $oldPassword = 'required|checkOldPassword[oldPassword]';
+                    $newPassword = 'required|min_length[8]|max_length[255]|is_unique[users.password]';
+                    $passwordConfirm = 'required|matches[newPassword]';
+                } else {
+                    $oldPassword = 'permit_empty';
+                    $newPassword = 'permit_empty';
+                    $passwordConfirm = 'permit_empty';
+                }
+            } else {
+                $oldPassword = 'permit_empty';
+                $newPassword = 'permit_empty';
+                $passwordConfirm = 'permit_empty';
+            }
+
+            if ($this->validate([
+                'username' => [
+                    'label' => 'Username',
+                    'rules' => $username,
+                    'errors' => [
+                        'required' => 'Field Username harus diisi',
+                        'is_unique' => 'Username sudah dipakai',
+                        'alpha_numeric' => 'Field Username hanya boleh berisi huruf dan angka',
+                        'min_length' => 'Minimum karakter untuk Field Username adalah 3 karakter',
+                        'max_length' => 'Maksimum karakter untuk Field Username adalah 50 karakter'
+                    ]
+                ],
+                'name' => [
+                    'label' => 'name',
+                    'rules' => 'required|alpha_space|max_length[255]|min_length[3]',
+                    'errors' => [
+                        'required' => 'Field Nama harus diisi',
+                        'alpha_space' => 'Field Nama hanya boleh berisi Huruf dan spasi',
+                        'max_length' => 'Maksimum karakter untuk Field Nama adalah 255 karakter',
+                        'min_length' => 'Minimum karakter untuk Field Nama adalah 3 karakter'
+                    ]
+                ],
+                'phone_no' => [
+                    'label' => 'Phone Number',
+                    'rules' => $hp,
+                    'errors' => [
+                        'is_unique' => 'Nomor hp sudah dipakai',
+                        'required' => 'Field Phone No harus diisi',
+                        'numeric' => 'Field Phone No hanya boleh berisi angka',
+                        'min_length' => 'Maksimum digit untuk Field Phone No adalah 7 karakter',
+                        'max_length' => 'Maksimum digit untuk Field Phone No adalah 15 karakter'
+                    ]
+                ],
+                'oldPassword' => [
+                    'label' => 'Old Password',
+                    'rules' => $oldPassword,
+                    'errors' => [
+                        'required' => 'Field Password Lama harus diisi',
+                        'checkOldPassword' => 'Password lama tidak sesuai',
+                    ]
+                ],
+                'newPassword' => [
+                    'label' => 'New Password',
+                    'rules' => $newPassword,
+                    'errors' => [
+                        'required' => 'Field New Password harus diisi',
+                        'min_length' => 'Minimum karakter untuk Field New Password adalah 8 karakter',
+                        'max_length' => 'Maksimum karakter untuk Field New Password adalah 255 karakter',
+                        'is_unique' => 'Password harus berbeda dari yang lama'
+                    ]
+                ],
+                'passwordConfirm' => [
+                    'label' => 'Confirm Password',
+                    'rules' => $passwordConfirm,
+                    'errors' => [
+                        'required' => 'Password harus di konfirmasi',
+                        'matches' => 'Password konfirmasi harus sama',
+                    ]
+                ],
+            ])) {
+                if (null !== ($this->request->getPost('newPassword'))) {
+                    // check old password
+                    // if new password is the same as old password, then do not update password
+                    if ($this->request->getPost('newPassword') != $this->request->getPost('oldPassword')) {
+                        $newPassword = $this->request->getPost('newPassword');
+                        $data = [
+                            'password' => $newPassword,
+                        ];
+                        $this->userModel->update(session()->get('id_user'), $data);
+                    }
+                }
+
+                $data = [
+                    'username' => $this->request->getPost('username'),
+                    'name' => $this->request->getPost('name'),
+                    'phone_no' => $this->request->getPost('phone_no'),
+                ];
+
+                session()->set($data);
+
+                $this->userModel->update(['id' => session()->get('id_user')], $data);
+                session()->setFlashdata('success', 'Profile berhasil diubah');
+                return redirect()->back();
+            } else {
+                $errors = $this->validator->getErrors();
+                return redirect()->back()->withInput()->with('errors', $errors);
+            }
+        }
+        return view('pages/profile', $data);
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('login');
+    }
+}
--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -37,13 +37,13 @@ $session = \Config\Services::session();
                         <div class="card-body">
                             <form method="POST" action="<?= base_url('login') ?>" class="needs-validation">
                                 <div class="form-group">
-                                    <label for="user">Email atau Username</label>
+                                    <label class="noselect" for="user">Email atau Username</label>
                                     <input id="user" type="text" class="form-control" name="user" tabindex="1" value="<?= $user ?>" required autofocus>
                                 </div>
 
                                 <div class="form-group">
                                     <div class="d-block">
-                                        <label for="password" class="control-label">Password</label>
+                                        <label class="noselect" for="password" class="control-label">Password</label>
                                     </div>
                                     <input id="password" type="password" class="form-control" name="password" tabindex="2" value="<?= $password; ?>" required>
                                     <div class="invalid-feedback">
@@ -54,7 +54,7 @@ $session = \Config\Services::session();
                                 <?php if (isset($validation)) { ?>
                                     <div class="col-12">
                                         <div class="alert alert-danger" role="alert">
-                                            <?php if ($validation->getError('user') == "Email tidak terdaftar") { ?>
+                                            <?php if (($validation->getError('user') === "Email tidak terdaftar") || ($validation->getError('user') === "Username tidak terdaftar")) { ?>
                                                 <?= $validation->getError('user') ?>
                                                 <br>
                                                 <a href="<?= base_url('register'); ?>" onclick="registerActive()">Create account</a>
@@ -66,15 +66,15 @@ $session = \Config\Services::session();
                                         </div>
                                     </div>
                                 <?php } else { ?>
-                                    <div class="forgot-password my-2">
-                                        <a href="<?= base_url('/forgot-password') ?>" id="forgotpass">Forgot Password?</a>
+                                    <div class="forgot-password my-2 ">
+                                        <a class="noselect" href="<?= base_url('/forgot-password') ?>" id="forgotpass">Forgot Password?</a>
                                     </div>
                                 <?php } ?>
 
-                                <div class="form-group my-3 d-inline">
+                                <div class="form-group my-3 d-inline noselect">
                                     <div class="custom-control custom-checkbox">
                                         <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember" <?= $remember ?>>
-                                        <label class="custom-control-label" for="remember">Remember Me(Unfinished)</label>
+                                        <label class="noselect" class="custom-control-label" for="remember">Remember Me(Unfinished)</label>
                                     </div>
                                 </div>
 
@@ -85,7 +85,7 @@ $session = \Config\Services::session();
                                 </div>
 
                             </form>
-                            <div class="mt-5 text-muted text-center">
+                            <div class="mt-5 text-muted text-center noselect">
                                 Don't have an account? <a href="<?= base_url('register'); ?>" onclick="registerActive()">Create One</a>
                             </div>
                         </div>
