15611313efe1e81eadd9ba70bb432a6154d6e526
RizalAnditama <rizalanditama2105@gmail.com>
2022-06-02 19:30:27 +0700

    adding favicon

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -2,15 +2,7 @@
 
 use App\Libraries\checkStatus;
 
-// setcookie('bruh', password_hash('bruh', PASSWORD_DEFAULT));
-// $pass = password_verify('bruh', $_COOKIE['bruh']);
-// if ($pass) {
-//     echo 'its right';
-// }
-// dd($pass);
-
 $check = new checkStatus();
-// $check->checkStatus();
 $check->checkCookies();
 
 ?>
@@ -23,6 +15,7 @@ $check->checkCookies();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="CI4app">
     <meta name="author" content="Rizal Anditama">
+    <link rel="icon" type="image/x-icon" href="https://i.imgur.com/cl0qVur.jpg">
 
     <!-- General CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
