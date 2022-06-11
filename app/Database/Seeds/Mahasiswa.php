fff762ab55226b08b600277bb05fd433fa5b3172
RizalAnditama <rizalanditama2105@gmail.com>
2022-06-11 20:07:14 +0700

    new seeder for mahasiswa

--- app/Database/Seeds/Mahasiswa.php
+++ app/Database/Seeds/Mahasiswa.php
@@ -18,15 +18,21 @@ class Mahasiswa extends Seeder
             $nim = $this->mhs->autonumber($jurusan);
             $tglLahir = $faker->dateTimeBetween('-30 years', '-20 years')->format('Y-m-d');
             $foto = $faker->imageUrl(200, 300, 'animals', true, 'cats');
+            $jenkel = ($faker->randomElements(['l', 'p']));
+            $agama = ($faker->randomElements(['islam', 'kristen', 'konghucu', 'hindu', 'buddha']));
+            $pend = ($faker->randomElements(['sd', 'smp', 'sma', 'smk', 's1']));
 
             $data = [
                 'nim_mhs' => $nim,
                 'nama_mhs' => $faker->firstName . ' ' . $faker->lastName,
+                'jenis_kelamin' => $jenkel,
                 'TmpLahir_mhs' => $faker->city,
                 'TglLahir_mhs' => $tglLahir,
+                'agama_mhs' => $agama,
                 'alamat_mhs' => $faker->address,
                 'hp_mhs' => $faker->e164PhoneNumber,
                 'jurusan_mhs' => $jurusan,
+                'pendidikan' => $pend,
                 'foto' => $foto,
             ];
 
