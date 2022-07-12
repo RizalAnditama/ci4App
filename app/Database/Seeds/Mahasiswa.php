8a64d6e589af5ca80ac2de339020f3fc119a3cd1
RizalAnditama <rizalanditama2105@gmail.com>
2022-07-12 19:25:50 +0700

    Beresin seeder fakernya

--- app/Database/Seeds/Mahasiswa.php
+++ app/Database/Seeds/Mahasiswa.php
@@ -14,18 +14,21 @@ class Mahasiswa extends Seeder
         $faker = \Faker\Factory::create('id_ID');
         for ($i = 0; $i < 100; $i++) {
 
+            $nama = $faker->firstName . ' ' . $faker->lastName;
             $jurusan = ($faker->randomElements(['sejarah', 'mipa', 'sastra']));
             $nim = $this->mhs->autonumber($jurusan);
-            $tglLahir = $faker->dateTimeBetween('-30 years', '-20 years')->format('Y-m-d');
-            $foto = $faker->imageUrl(200, 300, 'animals', true, 'cats');
+
             $jenkel = ($faker->randomElements(['l', 'p']));
+            $tglLahir = $faker->dateTimeBetween('-30 years', '-20 years')->format('Y-m-d');
             $agama = ($faker->randomElements(['islam', 'kristen', 'konghucu', 'hindu', 'buddha']));
-            $pend = ($faker->randomElements(['sd', 'smp', 'sma', 'smk', 's1']));
+
             $hp = str_replace('+', '0', $faker->e164PhoneNumber);
+            $pend = ($faker->randomElements(['SD', 'SMP', 'SMA', 'SMK', 'sS1']));
+            $foto = $faker->imageUrl(200, 300, 'animals', true, 'cats');
 
             $data = [
                 'nim_mhs' => $nim,
-                'nama_mhs' => $faker->firstName . ' ' . $faker->lastName,
+                'nama_mhs' => $nama,
                 'jenis_kelamin' => $jenkel,
                 'TmpLahir_mhs' => $faker->city,
                 'TglLahir_mhs' => $tglLahir,
